14671e7743a6ccc332ad6593e3e6a07178168a7a
Damian Yoo <dyoo@despegar.com>
2017-11-01 10:41:17 -0300

    tests for point 1

--- src/test/scala/com/despegar/neflis/solution/NeflisSpec.scala
+++ src/test/scala/com/despegar/neflis/solution/NeflisSpec.scala
@@ -4,10 +4,41 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class NeflisSpec extends FlatSpec with Matchers {
 
-  "User pepe" should "have seen estrella things" in new NeflisData {
-    val pepe = User(Set(film), estrellaThings.seasons.flatMap(_.episodes).toSet)
+  "User pepe" should "have seen estrella things - Series" in new NeflisData {
+    val pepe = User(Set(thor), estrellaThings.seasons.flatMap(_.episodes).toSet)
     val hasSeen = Neflis.userHasSeen(pepe, estrellaThings)
-    assert(hasSeen, "User should have seen estrella things")
+    hasSeen shouldBe true
+  }
+
+  "User pepa" should "have not seen all episodes of estrella things - Series" in new NeflisData {
+    val pepa = User(Set(thor), estrellaThings.seasons.head.episodes.toSet)
+    val hasSeen = Neflis.userHasSeen(pepa, estrellaThings)
+    hasSeen shouldBe false
+  }
+
+  "User pepi" should "have seen Thor - Film" in new NeflisData {
+    val pepi = User(Set(thor), Set())
+    val hasSeen = Neflis.userHasSeen(pepi, thor)
+    hasSeen shouldBe true
+  }
+
+  "User pepo" should "have seen episode one of estrella things - Episode" in new NeflisData {
+    val pepo = User(Set(thor), estrellaThings.seasons.head.episodes.toSet)
+    val hasSeen = Neflis.userHasSeen(pepo, season1.episodes.head)
+    hasSeen shouldBe true
+  }
+
+  "User pepo's brother" should "have seen season two of estrella things - Season" in new NeflisData {
+    val pepon = User(Set(thor), estrellaThings.seasons.flatMap(_.episodes).toSet)
+    val hasSeen = Neflis.userHasSeen(pepon, season2)
+    hasSeen shouldBe true
+  }
+
+  "User pepu" should "is too busy to watch any content - Film and Series" in new NeflisData {
+    val pepu = User(Set(), Set())
+    val (hasSeenFilm, hasSeenSeries) = (Neflis.userHasSeen(pepu, thor), Neflis.userHasSeen(pepu, estrellaThings))
+    hasSeenFilm shouldBe false
+    hasSeenSeries shouldBe false
   }
 
 }
@@ -18,8 +49,8 @@ trait NeflisData {
   val actors = List("Millie Bobby Brown")
   val defaultEpisode = Episode(60, 1, genre, actors, Nil)
   val season1 = Season(List(defaultEpisode, defaultEpisode.copy(order = 2), defaultEpisode.copy(order = 3)), 1, actors, genre)
-  val season2 = Season(List(defaultEpisode, defaultEpisode.copy(order = 2)), 1, actors, genre)
+  val season2 = Season(List(defaultEpisode.copy(duration = 59), defaultEpisode.copy(order = 2, duration = 59)), 1, actors, genre)
   val estrellaThings = Series(List(season1, season2), genre, actors)
 
-  val film = Film("Thor", 130, List("Chris Hemsworth", "Tom Hiddleston"), "Action")
+  val thor = Film("Thor", 130, List("Chris Hemsworth", "Tom Hiddleston"), "Action")
 }
